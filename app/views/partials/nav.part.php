16adf8bba951392e25d0d2384efe29909ae0816d
Rayepes <rayepes01@gmail.com>
2020-04-14 18:57:52 -0400

    inicio branch experimental

--- app/views/partials/nav.part.php
+++ app/views/partials/nav.part.php
@@ -9,7 +9,7 @@
               <span class="icon-bar"></span>
             </button>
             <a  class="navbar-brand page-scroll" href="#page-top">
-              <span>[PHOTO]</span>
+              <span>Menu Experimental</span>
             </a>
          </div>
          <div class="collapse navbar-collapse navbar-right" id="menu">
